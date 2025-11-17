b99ab719f2e001aa0e2dd05509c5e6d4f66cfaf9
Ricardo Henriques <paxcalpt@gmail.com>
2025-11-17 15:20:28 +0000

    rxiv-maker: update to version 1.9.2
    Updates the rxiv-maker formula to version 1.9.2.
    - Updated PyPI download URL
    - Updated SHA256 checksum

--- Formula/rxiv-maker.rb
+++ Formula/rxiv-maker.rb
@@ -1,8 +1,8 @@
 class RxivMaker < Formula
   desc "Convert Markdown to professional PDFs with automated figure generation"
   homepage "https://github.com/HenriquesLab/rxiv-maker"
-  url "https://files.pythonhosted.org/packages/cd/4a/44281122d7493a14f737b722d5711eddd00852e676e4f132e1a3b8c03402/rxiv_maker-1.9.1.tar.gz"
-  sha256 "76c92f5e68475032a4fa025d60f2e27217d750063a5b31854afd9ddf7ca04a5e"
+  url "https://files.pythonhosted.org/packages/e2/33/4e679a13d516ae09dcf2ac6ab6c5602b343c60cb46006578a98d5f16d403/rxiv_maker-1.9.2.tar.gz"
+  sha256 "d52930e0362c1b36ad0ceba01943aa377d0145a9e98dfc128effb549adbbedbb"
   license "MIT"
 
   depends_on "python@3.12"
