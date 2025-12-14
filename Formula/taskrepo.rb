53b94d0790f53127b3ae97a526b1ea887ed81cb9
Ricardo Henriques <paxcalpt@gmail.com>
2025-12-14 11:27:33 +0000

    taskrepo: update to version 0.10.12
    Updates the taskrepo formula to version 0.10.12.
    - Updated PyPI download URL
    - Updated SHA256 checksum

--- Formula/taskrepo.rb
+++ Formula/taskrepo.rb
@@ -2,8 +2,8 @@
 class Taskrepo < Formula
   desc "TaskWarrior-inspired task management with markdown files in git repositories"
   homepage "https://github.com/HenriquesLab/TaskRepo"
-  url "https://files.pythonhosted.org/packages/b9/63/1d323d963c149c5bdd9e7100eb7ab95bb4d476811d15d3d61bff498df406/taskrepo-0.10.11.tar.gz"
-  sha256 "4b111531af31029bb9b1000d91eedd52bf9b1bc7e166a108df31cf34fb4aa382"
+  url "https://files.pythonhosted.org/packages/d2/4d/63bb895dd945be1ce238d1214ef7aa684f1d2541d862d0e094727806c85e/taskrepo-0.10.12.tar.gz"
+  sha256 "2c1eb2e2ab8e927a24f41c3b6bcb3dc18dfc7be73cbaf15596ed52b74e58cfb0"
   license "MIT"
 
   depends_on "python@3.12"
