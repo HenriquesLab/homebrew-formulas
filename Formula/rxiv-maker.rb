25111e75ea3c8034ddcfed370abd0c93a2a17a42
Ricardo Henriques <paxcalpt@gmail.com>
2026-07-13 15:48:57 +0100

    rxiv-maker 1.22.2

--- Formula/rxiv-maker.rb
+++ Formula/rxiv-maker.rb
@@ -1,8 +1,8 @@
 class RxivMaker < Formula
   desc "Convert Markdown to professional PDFs with automated figure generation"
   homepage "https://github.com/HenriquesLab/rxiv-maker"
-  url "https://files.pythonhosted.org/packages/63/71/40b6a1927d74339f1c466e3adb1b54b0f05d877c0aba7eee66cbec101bf0/rxiv_maker-1.22.1.tar.gz"
-  sha256 "d6cb619ff2376d0292d36031a355cc89473a8a78f46faeb7624211af5e183975"
+  url "https://files.pythonhosted.org/packages/c6/36/ffd7a2da7e349391e526c6122540d1d35ea0ec061203504b3bd02cd35834/rxiv_maker-1.22.2.tar.gz"
+  sha256 "ac6f1d9ddb043804d7a3a18d44a4f4a9bf93f66bd72ea23163c2d1a61cae1957"
   license "MIT"
 
   depends_on "gh"
@@ -14,7 +14,7 @@ class RxivMaker < Formula
 
   def install
     venv = libexec/"venv"
-    system Formula["python@3.13"].opt_bin/"python3.13", "-m", "venv", venv
+    system formula_opt_bin("python@3.13")/"python3.13", "-m", "venv", venv
     system venv/"bin/pip", "install", "-v", "--ignore-installed",
            build.head? ? "git+." : "."
     bin.install_symlink venv/"bin/rxiv"
