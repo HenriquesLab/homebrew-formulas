b29249e7c37d7798b8e6f278e1ab03ed671acad5
Ricardo Henriques <paxcalpt@gmail.com>
2026-09-15 16:20:12 +0100

    rxiv-maker 1.24.3 (#14)

--- Formula/rxiv-maker.rb
+++ Formula/rxiv-maker.rb
@@ -1,8 +1,8 @@
 class RxivMaker < Formula
   desc "Convert Markdown to professional PDFs with automated figure generation"
   homepage "https://github.com/HenriquesLab/rxiv-maker"
-  url "https://files.pythonhosted.org/packages/ea/db/a9c264c8c50804e002079443b79913def4b6ecdf7f66a99b270985997829/rxiv_maker-1.24.2.tar.gz"
-  sha256 "89d8b70dfb9f7f0aa480b79986df43efccf4c7ef761ee04062fd2c791cde342b"
+  url "https://files.pythonhosted.org/packages/74/f0/c43f3cdce616a1cdc55f3982f32f387298c4ebedf34773f5de95c78c6c97/rxiv_maker-1.24.3.tar.gz"
+  sha256 "389b7d62f1242468fffe40247665cb685a06d5bbe6f9a03870c18624c10a211c"
   license "MIT"
 
   depends_on "gh"
